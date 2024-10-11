21e967828637634390e5b496b1a4fe361b368cec
philluple <pnl2111@columbia.edu>
2024-10-11 12:03:04 -0400

    Currently working but using the find command

--- CSVeaseLexer.py
+++ CSVeaseLexer.py
@@ -1,6 +1,6 @@
 import sys
 
-from Exception import InvalidString, LexerErrors, BadIdentifier, UnexpectedCharacter, InvalidSequence
+from Exception import InvalidString, BadIdentifier, UnexpectedCharacter, InvalidSequence
 from Error import LexerErrors
 
 # TODO: do the error handling @ Phillip
@@ -16,17 +16,21 @@ class Line:
         self.current_position = 0
         self.current_char = self.current_line[self.current_position] if self.current_line else None
         self.prev_class = None
+        
         self.OP_TOKENS = {
           '=': 'EQ',
           '+': 'PLUS',
-          '(': 'LPAR',
-          ')': 'RPAR',
         }
-        self.KEYWORDS = ['SHOW', 'LOAD', 'CONVERT', 'ROWS', 'COLUMNS', 'GET', 'IN', 'TO', 'OUTPUT', 'AS', 'PDF', 'CSV', 'JPEG']
+        
+        self.KEYWORDS = ['SHOW', 'LOAD', 'CONVERT', 'ROWS', 'COLUMNS', 'GET', 'IN', 'TO', 'OUTPUT', 'AS', 'GROUP_BY', 'PDF', 'CSV', 'JPEG']
+        
         self.MODIFIERS = {
             ',': 'COMMA',
             '.': 'DOT',
+            '(': 'LPAR',
+            ')': 'RPAR',
         }
+        
         
     def advance(self):
       self.current_position += 1
@@ -41,49 +45,31 @@ class Line:
             self.current_char = None
         else:
             self.current_char = self.current_line[self.current_position]
-            
-    def matchKeyWord(self,start,length,targetString):
-        if self.current_line[start:start+length] == targetString:
-            return start+length
-        else:
-            return start
 
-    def isKeyword(self):
-        if self.current_char == 'S':
-            self.end_position = self.matchKeyWord(self.current_position,4,"SHOW")
-            if self.end_position > self.current_position:
-                return True
-            else:
-                return False
-        elif self.current_char == 'I':
-            self.end_position = self.matchKeyWord(self.current_position,2,"IN")
-            if self.end_position > self.current_position:
-                return True
-            else:
-                return False
-        else:
-            return False
-        
     def resolve_integer(self):
+        print(f"Call to resolve integer: {self.current_char}")
         num_str = ''
         while  self.current_char is not None:
             # We reached the end of the sequence and we want to return the integer
             if self.current_char.isspace() or self.current_char == ')':
                 self.advance()
-                return ('INTEGER', num_str)           
             if self.current_char.isalpha():
                 raise InvalidSequence(self.current_line)
             num_str += self.current_char
             self.advance()
-          
+        return ('INTEGER', num_str)     
+    
     def check_prev(self):
-      return (self.prev_class == None or self.prev_class == "WHITESPACE" or self.prev_class == "OPERATOR")
+        truth = None
+        if (self.prev_class == None or self.prev_class == "WHITESPACE" or self.prev_class == "OPERATOR"):
+            truth = True
+        else:
+            truth = False
+        return truth
     
     def valid_operator(self):
-      if self.current_position == len(self.current_line) - 1 or self.check_prev() or self.current_char == ")":
-          return True
+      return (self.current_position == len(self.current_line) - 1 or self.check_prev() or self.current_char == ")")
                 
-      return False
     def find_next_index(self):
         next_index =  self.current_line.find(' ', self.current_position)
         
@@ -106,6 +92,10 @@ class Line:
         return next_index
           
     def process_line(self):
+        # Allows for line comments
+        if "//" in self.current_line:
+            return []
+        
         while self.current_char is not None:
             next_space = self.find_next_index()
             if next_space == -1:  # No more spaces found
@@ -120,7 +110,7 @@ class Line:
                 continue
             
             # checking for keyword -- Highest priority
-            if unit.upper() in self.KEYWORDS and self.check_prev():
+            elif unit.upper() in self.KEYWORDS and self.check_prev():
                 self.line_tokens.append(('KEYWORD',unit.upper()))
                 self.current_position = self.current_line.find(' ', self.current_position)
                 if self.current_position >= len(self.current_line) or self.current_position == -1:
@@ -131,7 +121,7 @@ class Line:
                 continue 
               
             # checking for identifier -- Second priority
-            elif self.current_char.isalpha() and self.check_prev():
+            elif self.current_char.isalpha() and (self.check_prev() or self.prev_class == "MODIFIER"):
               self.line_tokens.append(('IDENTIFIER', unit))
               self.current_position = next_space
               if self.current_position >= len(self.current_line) or self.current_position == -1:
@@ -148,7 +138,9 @@ class Line:
                 self.advance()
                 continue 
             
+            # checking for modifier 
             elif self.current_char in self.MODIFIERS:
+                
                 self.line_tokens.append((self.MODIFIERS[self.current_char], self.current_char))
                 self.prev_class = 'MODIFIER'
                 self.advance()
@@ -165,14 +157,14 @@ class Line:
             elif self.current_char == '"':
                 next_quot = self.current_line.find('"', self.current_position + 1)  # Start searching from next position
                 if next_quot == -1:  # No closing quote found
-                    raise InvalidString(f"No closing quote found for string starting at position {self.current_position}")
+                    raise InvalidString("Unterminated string literal.")
                 string_value = self.current_line[self.current_position + 1: next_quot]  # Exclude the quotes
                 self.line_tokens.append(('STRING', string_value))
                 self.current_position = next_quot + 1  # Move past the closing quote
                 self.advance()  # Update the current character
                 continue
             else: 
-                raise UnexpectedCharacter
+                raise UnexpectedCharacter(self.current_char)
           
         return self.line_tokens
             
@@ -195,6 +187,7 @@ class CSVeaseLexer:
                     line_to_process = Line(line)
                     self.tokens.extend(line_to_process.process_line())
                 except UnexpectedCharacter as e:
+                    print(e)
                     self.errors.UnexpectedCharacter(line, self.current_line_no)
                 except BadIdentifier as e:
                     self.errors.BadIdentifier(line, self.current_line_no)
@@ -204,11 +197,25 @@ class CSVeaseLexer:
                     self.errors.InvalidSequence(line, self.current_line_no)
     
     def print_tokens(self):
-      for token in self.tokens:
-        token_class, value = token
-        if token_class == "WHITESPACE":
-            continue
-        print(f"<{token_class}, '{value}'>")
+        count = {}
+        for token in self.tokens:
+            token_class, value = token
+            if token_class not in count:
+                count[token_class] = [token]
+                
+            else: 
+                count[token_class].append(token)
+
+        print(f"{len(self.tokens)} TOKENS: ")
+        for key in count.keys():
+            print(f"\n{key} ({len(count[key])})")
+            if key == "WHITESPACE":
+                print(f"    {count[key][0]}")
+                print(f"            ...")
+                print(f"    {count[key][0]}")
+                continue
+            for value in count[key]:
+                print(f"    {value}")
         
 
 if __name__ == '__main__':
@@ -224,29 +231,3 @@ if __name__ == '__main__':
         lexer.errors.printErrors()
     else:
         lexer.print_tokens()
-    
-# <IDENTIGIER, >
-# // this will load a csv file and create an instance of the csv file as table2 
-
-# table1 = load class_roster.csv
-
-# // output rows and columns
-
-# show rows table1
-# show columns	table1
-
-# // select function
-
-# sub_table = get (NAME, AGE, YEAR) IN table1
-
-# // selecting the year column, getting the average age of each year in the table and creating a new group table -> example below
-
-# group_table = avg(age) group by (YEAR) IN sub_table
-
-# // creates a new csv file called group_table.csv with those changes
-# chart1 = convert group_table to bar chart
-
-
-# output group_table TO group_table_file as CSV 
-# output group_table TO group_table_file as PDF
-# output group_table TO chart1 as JPEG 
