833171e8d0a3c02c8e3f093beed6b70a6251d623
philluple <pnl2111@columbia.edu>
2024-11-10 09:05:30 -0500

    Better printing

--- Parser.py
+++ Parser.py
@@ -1,54 +1,19 @@
-from typing import Any, Dict, List, Optional, Tuple
 from Error import ParserError
-from enum import Enum, auto
 
-class TokenType(Enum):
-    LOAD = auto()
-    DATA = auto()
-    STRING = auto()
-    INTO = auto()
-    IDENTIFIER = auto()
-    SHOW = auto()
-    ROWS = auto()
-    COLUMNS = auto()
-    IN = auto()
-    GET = auto()
-    INTEGER = auto()
-    FROM = auto()
-    PLUS = auto()
-    EQ = auto()
-    EOF = auto()
-
-@dataclass
-class Token:
-    type: TokenType
-    value: str
-    line: int
-    column: int
-
-class ASTNode:
-    type: str
-    value: Any
-    children: List['ASTNode']
-    token: Optional[Token] = None
+class Node:
+    def __init__(self, token, children):
+        self.token = token
+        self.children = children if children else []
+        
+    def __repr__(self):
+        """Returns a string representation of the node and its children"""
+        token_type, token_value = self.token
+        node_str = token_type
+        if token_value is not None and token_value != token_type:
+            node_str += f"({token_value})"
+            
+        return node_str
     
-    def __str__(self):
-        return self._str_helper()
-    
-    def _str_helper(self, level=0):
-        indent = "  " * level
-        result = f"{indent}{self.type}"
-        if self.value:
-            result += f": {self.value}"
-        if self.children:
-            result += "\n" + "\n".join(child._str_helper(level + 1) for child in self.children)
-        return result
-
-# class Node:
-#     def __init__(self, token, children):
-#         self.token = token
-#         self.children = children
-
 class Entry:
     # if it is a terminal, value is the token, if it is not a terminal, value is a string
     def __init__(self, isTerminal, value):
@@ -62,93 +27,38 @@ class Parser:
             raise ParserError("Empty token stream")
         if tokens[-1][0] != '$':
             raise ParserError("Token stream must end with EOF ($) token")
-        self.tokens = [Token(TokenType[type_], value, line, col) for type_, value, line, col in tokens]
+        self.tokens = tokens
         self.current = 0
 
-        self.LL1: Dict[Tuple[str, TokenType], List[Entry]] = {
-            ("S", TokenType.LOAD): [
-                Entry(True, (TokenType.LOAD, "LOAD")), 
-                Entry(False, "A")
-            ],
-            ("A", TokenType.DATA): [
-                Entry(True, (TokenType.DATA, "DATA")), 
-                Entry(False, "Aa")
-            ],
-            ("A", TokenType.STRING): [
-                Entry(True, (TokenType.STRING, None)), 
-                Entry(False, "Aa")
-            ],
-            ("Aa", TokenType.INTO): [
-                Entry(True, (TokenType.INTO, "INTO")), 
-                Entry(False, "Aaa")
-            ],
-            ("Aaa", TokenType.IDENTIFIER): [
-                Entry(True, (TokenType.IDENTIFIER, None)), 
-                Entry(False, "Aaaa")
-            ],
-            ("Aaaa", TokenType.EOF): None,
-            
-            ("S", TokenType.SHOW): [
-                Entry(True, (TokenType.SHOW, "SHOW")), 
-                Entry(False, "B")
-            ],
-            ("B", TokenType.ROWS): [
-                Entry(True, (TokenType.ROWS, "ROWS")), 
-                Entry(False, "Ba")
-            ],
-            ("B", TokenType.COLUMNS): [
-                Entry(True, (TokenType.COLUMNS, "COLUMNS")), 
-                Entry(False, "Bb")
-            ],
-            ("Ba", TokenType.IN): [
-                Entry(True, (TokenType.IN, "IN")), 
-                Entry(False, "Baa")
-            ],
-            ("Baa", TokenType.IDENTIFIER): [
-                Entry(True, (TokenType.IDENTIFIER, None)), 
-                Entry(False, "Baaa")
-            ],
-            ("Baaa", TokenType.EOF): None,
-            
-            ("S", TokenType.GET): [
-                Entry(True, (TokenType.GET, "GET")), 
-                Entry(False, "C")
-            ],
-            ("C", TokenType.INTEGER): [
-                Entry(True, (TokenType.INTEGER, None)), 
-                Entry(False, "Ca")
-            ],
-            ("C", TokenType.IDENTIFIER): [
-                Entry(True, (TokenType.IDENTIFIER, None)), 
-                Entry(False, "Cb")
-            ],
-            ("Ca", TokenType.ROWS): [
-                Entry(True, (TokenType.ROWS, "ROWS")), 
-                Entry(False, "Caa")
-            ],
-            ("Caa", TokenType.FROM): [
-                Entry(True, (TokenType.FROM, "FROM")), 
-                Entry(False, "Caaa")
-            ],
-            ("Cb", TokenType.PLUS): [
-                Entry(True, (TokenType.PLUS, "PLUS")), 
-                Entry(False, "C")
-            ],
-            ("Cb", TokenType.FROM): [
-                Entry(True, (TokenType.FROM, "FROM")), 
-                Entry(False, "Cba")
-            ],
-            
-            ("S", TokenType.IDENTIFIER): [
-                Entry(True, (TokenType.IDENTIFIER, None)), 
-                Entry(False, "X")
-            ],
-            ("X", TokenType.EQ): [
-                Entry(True, (TokenType.EQ, "=")), 
-                Entry(False, "S")
-            ]
-        }
+        self.LL1 = {("S", 'LOAD'): [Entry(True, ('LOAD', 'LOAD')), Entry(False, "A")],
+                    ("A", 'DATA'): [Entry(True, ('DATA', 'DATA')), Entry(False, "Aa")],
+                    ("A", 'STRING'): [Entry(True, ('STRING', None)), Entry(False, "Aa")],
+                    ("Aa", '$'): None,
+                    ("Aa",'INTO'): [Entry(True, ('INTO', 'INTO')), Entry(False, "Aaa")],
+                    ("Aaa",'IDENTIFIER'):[Entry(True, ('IDENTIFIER', None)), Entry(False, "Aaaa")],
+                    ("Aaaa", '$'): None,
+                    ("S", 'SHOW'): [Entry(True, ('SHOW', 'SHOW')), Entry(False, "B")],
+                    ("B", 'ROWS'): [Entry(True, ('ROWS', 'ROWS')), Entry(False, "Ba")],
+                    ("B", 'COLUMNS'): [Entry(True, ('COLUMNS', 'COLUMNS')), Entry(False, "Bb")],
+                    ("Ba", 'IN'): [Entry(True, ('IN', 'IN')), Entry(False, "Baa")],
+                    ("Baa", 'IDENTIFIER'): [Entry(True, ('IDENTIFIER', None)), Entry(False, "Baaa")],
+                    ("Baaa", '$'): None,
+                    ("Bb", 'IN'): [Entry(True, ('IN', 'IN')), Entry(False, "Bba")],
+                    ("Bba", 'IDENTIFIER'): [Entry(True, ('IDENTIFIER', None)), Entry(False, "Bbaa")],
+                    ("Bbaa", '$'): None,
+                    ("S", "GET"): [Entry(True, ("GET", "GET")), Entry(False, "C")],
+                    ("C", "INTEGER"): [Entry(True, ("INTEGER", None)), Entry(False, "Ca")],
+                    ("Ca", "ROWS"): [Entry(True, ("ROWS", "ROWS")), Entry(False, "Caa")],
+                    ("Caa", "FROM"): [Entry(True, ("FROM", "FROM")), Entry(False, "Caaa")],
+                    ("C", "IDENTIFIER"): [Entry(True, ("IDENTIFIER", None)), Entry(False, "Cb")],
+                    ("Cb", "PLUS"): [Entry(True, ("PLUS", "PLUS")), Entry(False, "C")],
+                    ("Cb", "FROM"): [Entry(True, ("FROM", "FROM")), Entry(False, "Cba")],
+                    ("Cba", "IDENTIFIER"): [Entry(True, ("IDENTIFIER", None)), Entry(False, "Cbaa")],
+                    ("Cbaa", '$'): None,
+                    ('S', 'IDENTIFIER'): [Entry(True, ('IDENTIFIER', None)), Entry(False, 'X')],
+                    ('X', 'EQ'): [Entry(True, ('EQ', '=')), Entry(False, 'S')],
 
+                    }
         
     def peek(self):
         if self.current >= len(self.tokens):
@@ -177,8 +87,7 @@ class Parser:
                 if token[1] is None or currentToken[1] == token[1]:
                     self.advance()
                     return True
-        expected = ', '.join(f"{t[0]}({t[1]})" if t[1] else t[0] for t in tokenList)
-        raise ParserError(f"Expected one of: {expected}, but got: {currentToken[0]}({currentToken[1]})", currentToken)
+        return False
     
     def parse(self):
         return self.expression()
@@ -200,6 +109,23 @@ class Parser:
                     ret_list.append(node)
         return ret_list
 
+    def print_ast(self, nodes, level=0):
+        if not nodes:
+            return ""
+        
+        indent = "  " * level
+        result = indent + "["
+        
+        # Print first node
+        result += str(nodes[0])
+        
+        # If there are children (nodes[1]), print them recursively
+        if len(nodes) > 1 and nodes[1]:
+            result += ",\n" + self.print_ast(nodes[1], level + 1)
+            
+        result += "]"
+        return result
+    
     def buildASTfromLL1(self):
         terminal = 'S'
         currentToken = self.peek()
@@ -209,13 +135,25 @@ class Parser:
 
 
 if __name__=='__main__':
-    ast = Parser([('LOAD','LOAD'),('DATA','DATA'),('INTO','INTO'),('IDENTIFIER','sales_data'), ('$', '$')]).buildASTfromLL1()
-    print(ast)
-    ast = Parser([('LOAD','LOAD'),('STRING','data.csv'), ('$', '$')]).buildASTfromLL1()
-    print(ast)
-    ast = Parser([('IDENTIFIER','table1'),('EQ','='), ('LOAD', 'LOAD'), ("STRING", 'data.csv'), ('$', '$')]).buildASTfromLL1()
-    print(ast)
-    ast = Parser([('GET','GET'),('IDENTIFIER','COLUMN1'), ('PLUS', '+'), ("IDENTIFIER", 'COLUMN2'), ('PLUS', '+'), ("IDENTIFIER", 'COLUMN3'), ("FROM", 'FROM'), ("IDENTIFIER", 'TABLE1'),('$', '$')]).buildASTfromLL1()
-    print(ast)
+    test_cases = [
+        [('LOAD','LOAD'),('DATA','DATA'),('INTO','INTO'),('IDENTIFIER','sales_data'), ('$', '$')],
+        [('LOAD','LOAD'),('STRING','data.csv'), ('$', '$')],
+        [('IDENTIFIER','table1'),('EQ','='), ('LOAD', 'LOAD'), ("STRING", 'data.csv'), ('$', '$')],
+        [('GET','GET'),('IDENTIFIER','COLUMN1'), ('PLUS', '+'), ("IDENTIFIER", 'COLUMN2'), 
+         ('PLUS', '+'), ("IDENTIFIER", 'COLUMN3'), ("FROM", 'FROM'), ("IDENTIFIER", 'TABLE1'),('$', '$')]
+    ]
+    
+    print("Testing Parser with different inputs:")
+    print("=" * 50)
+    
+    for i, tokens in enumerate(test_cases, 1):
+        print(f"\nTest Case {i}:")
+        print("-" * 20)
+        print("Input tokens:", tokens)
+        print("\nResulting AST:")
+        parser = Parser(tokens)
+        ast = parser.buildASTfromLL1()
+        print(parser.print_ast(ast))
+        print("=" * 50)
 
 
